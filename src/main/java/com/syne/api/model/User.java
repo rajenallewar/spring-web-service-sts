071fad3e137258312d9e8f7546419d8a50573940
Rajendra Allewar <Rajendra.Allewar@synechron.com>
2019-12-18 13:56:54 +0000

    Spring boot app to connect to maria DB, CORS config, swagger, filter

---
+++ src/main/java/com/syne/api/model/User.java
@@ -0,0 +1,43 @@
+package com.syne.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@Table(name = "user_info")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	
+	@Id
+	private String userId;
+	private String userName;
+	private String userAddress;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserAddress() {
+		return userAddress;
+	}
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+	
+}
